711cb8b4fa91b78c3dd11fb5adb204cead427d83
Ulmanb <barulman@gmail.com>
2016-10-01 00:32:44 +0300

    readme and theoretical symbol support

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # react-state-wrapper
+
+Are you tired of `this.setState()` everywhere?
+this one is for you!
+
+## what
+
+Extremely simple and tiny utility with no external dependencies to never write this.setState again.
+
+## usage
+
+Import the constructor function:
+
+	import Wrapper from 'rootToDependency';
+
+In the react component whose state you want to wrap constructor's create the wrapper using the imported function and sending this as a parameter:
+
+    constructor(props) {
+      super(props);
+
+      // Initialize state
+      this.state = {
+        loggedIn: null
+      };
+
+      // Create the state wrapper
+      this.stateWrapper = new Wrapper(this);
+    }
+
+Then you can just replace any access to state with the new wrapper object.
+
+Getting state data:
+
+    let isLoggedIn = this.stateWrapper.loggedIn;
+Setting state data:
+
+    this.stateWrapper.loggedIn = false;
+And thats it.
+The state wrapper will call `this.setState({ loggedIn: false })` for you.
+
+Enjoy 😎
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
+// Constructor function
 export default function (component) {
   const { state } = component;
 
-  for (const prop in state) {
-    if ({}.hasOwnProperty.call(state, prop)) {
-      Object.defineProperty(this, prop, {
-        get: (function () {
-          return this.state[prop];
-        }).bind(component),
-        set: (function (value) {
-          const stateSetter = {};
-          stateSetter[prop] = value;
-          this.setState(stateSetter);
-        }).bind(component),
-        enumerable: false,
-        configurable: false
+  Object.getOwnPropertyNames(state).concat(Object.getOwnPropertySymbols(state))
+  .forEach(prop => {
+    // Define name identical properties to wrap state
+    Object.defineProperty(this, prop, {
+      get: (function () {
+        return this.state[prop];
+      }).bind(component),
+      set: (function (value) {
+        const stateSetter = {};
+        stateSetter[prop] = value;
+        this.setState(stateSetter);
+      }).bind(component),
+      enumerable: false,
+      configurable: false
       });
-    }
-  }
+  });
 
   return this;
 }
